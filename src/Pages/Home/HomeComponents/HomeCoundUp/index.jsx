315c37c4f2e4b8a4f44a5ee17b171ebb8dae9841
AllyintheCode <allyasadzade@gmail.com>
2025-05-27 19:21:11 +0400

    add

--- src/Pages/Home/HomeComponents/HomeCoundUp/index.jsx
+++ src/Pages/Home/HomeComponents/HomeCoundUp/index.jsx
@@ -4,15 +4,20 @@ import { IoLibrarySharp } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { HiMiniBuildingLibrary } from "react-icons/hi2";
 import { LiaBookSolid } from "react-icons/lia";
-
+import { useInView } from 'react-intersection-observer'
 const HomeCoundUp = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.3,
+  })
   return (
     <div className='bg-[#008186] '>
       <div className='flex-col h-75 gap-2 lg:max-w-[1200px] mx-auto flex md:gap-3 md:flex-row items-center md:justify-center text-4xl md:h-34 lg:justify-between text-[#fcf2dc]'>
         <div>
-          <div className='flex items-center gap-2'>
+          <div ref={ref} className='flex items-center gap-2'>
             <HiMiniBuildingLibrary />
-            <CountUp end={10000} />+
+            {inView && < CountUp end={10000} suffix='+' />}
+            {!inView && 0}
           </div>
           <p className='text-xl ml-8'>Books in Library
           </p>
@@ -20,7 +25,8 @@ const HomeCoundUp = () => {
         <div >
           <div className='flex items-center gap-2 '>
             <FaUser />
-            <CountUp end={10000} />+
+            {inView && < CountUp end={10000} suffix='+' />}
+            {!inView && 0}
           </div>
           <p className='text-xl ml-8'>
             Active Users
@@ -29,7 +35,9 @@ const HomeCoundUp = () => {
         <div >
           <div className='flex items-center gap-2 '>
             < IoLibrarySharp />
-            <CountUp end={25} />+
+            {inView && < CountUp end={25} suffix='+' delay={0.7} />}
+            {!inView && 0}
+
           </div>
           <p className='text-xl ml-8'>
             Genres
@@ -38,7 +46,8 @@ const HomeCoundUp = () => {
         <div >
           <div className='flex items-center gap-2 '>
             <  LiaBookSolid />
-            <CountUp end={500000} />+
+            {inView && < CountUp end={500000} suffix='+' />}
+            {!inView && 0}
           </div>
           <p className='text-xl ml-8'>
             Books Available
