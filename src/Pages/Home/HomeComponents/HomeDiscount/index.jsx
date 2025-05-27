a3b1bd7ba2debcf8ab8391d8d2bc05cda43c7f24
AllyintheCode <allyasadzade@gmail.com>
2025-05-27 19:04:04 +0400

    responsive complated

--- src/Pages/Home/HomeComponents/HomeDiscount/index.jsx
+++ src/Pages/Home/HomeComponents/HomeDiscount/index.jsx
@@ -3,31 +3,38 @@ import { Link } from 'react-router-dom'
 
 const HomeDiscount = () => {
   return (
-    <div className='max-w-[1200px] mx-auto mt-20'>
-      <div className='flex justify-between'>
-        <div className='max-w-[588px] max-h-[288px] bg-[#008186] flex p-[50px] justify-between items-center relative '>
-          <div className='max-w-[172px] max-h-[118px]'>
+    <div className='max-w-[1200px] mx-auto mt-20 px-4'>
+      <div className='flex flex-col gap-10 lg:flex-row lg:justify-between'>
+        {/* Box 1 */}
+        <div className='w-full lg:w-[49%] bg-[#008186] flex flex-col sm:flex-row p-[30px] sm:p-[50px] justify-between items-center relative'>
+          <div className='max-w-[172px] max-h-[118px] mb-6 sm:mb-0'>
             <img src="/sale.png" alt="" />
           </div>
-          <div>
-            <h2 className='max-w-[280px] text-3xl text-amber-50 font-[600]'>
-              Special sale on Featured books !
+          <div className='text-center sm:text-left'>
+            <h2 className='text-3xl text-amber-50 font-[600]'>
+              Special sale on Featured books!
             </h2>
-            <Link className='flex border-[#d37643] bg-[#d37643] max-w-[125px] py-[14px] px-[24px] justify-center items-center text-white hover:border hover:border-[#d37643] hover:bg-[#fcf2dc] hover:text-[#d37643] duration-300 text-sm mt-5'>Shop Now</Link>
+            <Link className='inline-block border-[#d37643] bg-[#d37643] py-[14px] px-[24px] text-white hover:border hover:border-[#d37643] hover:bg-[#fcf2dc] hover:text-[#d37643] duration-300 text-sm mt-5'>
+              Shop Now
+            </Link>
           </div>
-          <img src="/discount.svg" alt="" className='absolute top-[-29px] right-[-27px]' />
+          <img src="/discount.svg" alt="" className='absolute top-[-19px] right-[-15px]  w-[70px] sm:w-[100px] sm:top-[-29px] sm:right-[-27px]' />
         </div>
-        <div className='max-w-[588px] max-h-[288px] bg-[#e7b490] flex p-[50px] justify-between items-center  relative'>
-          <div className='max-w-[172px] max-h-[118px]'>
+
+        {/* Box 2 */}
+        <div className='w-full lg:w-[49%] bg-[#e7b490] flex flex-col sm:flex-row p-[30px] sm:p-[50px] justify-between items-center relative'>
+          <div className='max-w-[172px] max-h-[118px] mb-6 sm:mb-0'>
             <img src="/basket.png" alt="" />
           </div>
-          <div>
-            <h2 className='max-w-[280px] text-3xl text-[#008186] font-[600]'>
+          <div className='text-center sm:text-left'>
+            <h2 className='text-3xl text-[#008186] font-[600]'>
               Checkout for a discount up to 20%
             </h2>
-            <Link className='flex border-[#008186] bg-[#d37643] max-w-[150px] py-[14px] px-[24px] justify-center items-center text-white hover:border hover:border-[#d37643] hover:bg-[#fcf2dc] hover:text-[#d37643] duration-300 text-sm mt-5'>Checkout Now</Link>
+            <Link className='inline-block border-[#008186] bg-[#d37643] py-[14px] px-[24px] text-white hover:border hover:border-[#d37643] hover:bg-[#fcf2dc] hover:text-[#d37643] duration-300 text-sm mt-5'>
+              Checkout Now
+            </Link>
           </div>
-          <img src="/discount2.svg" alt="" className='absolute top-[-29px] right-[-27px]' />
+          <img src="/discount2.svg" alt="" className='absolute wtop-[-19px] right-[-15px] sm:top-[-29px] sm:right-[-27px] w-[70px] sm:w-[100px]' />
         </div>
       </div>
     </div>
